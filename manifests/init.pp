9071d8ba6f4a419dfb12d721d868dc5c56d3fa62
cesarpball <cesarpball@localhost.localdomain>
2016-12-20 13:27:39 +0000

    add manifests and template

---
+++ manifests/init.pp
@@ -0,0 +1,41 @@
+# vim:set noet:
+# vim:set sts=8 ts=8:
+# vim:set shiftwidth=8:
+
+# = Class: ilo 
+#
+# iLO management from puppet. 
+#
+# == Parameters
+#
+# == Examples
+#
+# Usage:
+#  include ilo 
+#
+# == Authors
+#
+# - Cesar Prieto Ballester <cesar.ballester@piksel.com>
+#
+# == Copyright
+#
+# Copyright 2016 Piksel
+#
+class ilo {
+
+	$username = hiera('ilo::ilo_username')
+	$password = chomp(symmetric_decrypt(hiera('ilo::ilo_password')))
+
+	file { '/opt/hp/puppet-ilo.conf':
+	  ensure  => file,
+	  audit => 'content',
+	  notify => Exec['reset_ilo_password'],
+	  content => template('ilo/puppet-ilo.conf.erb'),
+	}
+
+	exec { 'reset_ilo_password':
+	  command     => '/sbin/hponcfg -f /opt/hp/puppet-ilo.conf',
+	}
+
+}
+
